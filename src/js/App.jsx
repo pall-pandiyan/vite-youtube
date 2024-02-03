783dcec43275c08117cd1e566578f2f19086401e
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-02-03 23:11:06 +0530

    save changes

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from "react";
+import colors from '../json/colors.json';
+
 function App() {
+
+  // const [colors, setColors] = useState(null)
+  // useEffect(() => {
+  //   const fetchColors = async() => {
+  //     try {
+  //       const respose = await fetch("/home/rx7/projects/training/react/vite/vite-youtube/src/json/colors.json");
+  //       const data = respose.json();
+  //       setColors(data);
+  //     }
+  //     catch (error) {
+  //       console.error("Error reading JSON file");
+  //     }
+  //   };
+
+  //   console.log(colors);
+  //   fetchColors();
+  // }, []);
 
 
   return (
-    <>
-      <h1 className="bg-red-200 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className={colors.home_page.background}>
+      <h1>hello</h1>
+    </div>
+
   )
 }
+
 
 export default App
