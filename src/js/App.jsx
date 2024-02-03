9eaba504ae43868195939b6a7eb8b820d4ff18bd
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-02-03 20:21:42 +0530

    tested the tailwind

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from '../img/react.svg'
-import viteLogo from '/vite.svg'
-import '../css/App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="bg-red-200 text-3xl font-bold underline">
         Hello world!
       </h1>
     </>
